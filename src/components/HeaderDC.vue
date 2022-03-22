855326ac9879a6167a1782d710f5a4b9cd1b6a3f
MattFofo <matteo.segattini2@gmail.com>
2022-03-22 16:14:52 +0100

    struttura base header

--- src/components/HeaderDC.vue
+++ src/components/HeaderDC.vue
@@ -1,15 +1,85 @@
 <template>
     <header>
-        header
+    <div class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li v-for="(link, index) in links" :key="index">
+                <a :href="link.href">
+                    {{ link.text }}
+                </a>
+            </li>
+        </ul>
+    </div>
     </header>
 </template>
 
 <script>
 export default {
-    name: 'HeaderDC'
+    name: 'HeaderDC',
+    data() {
+        return {
+            links: [
+                {
+                    href: '#',
+                    text: 'CHARACTERS'
+                },
+                {
+                    href: '#',
+                    text: 'COMICS'
+                },
+                {
+                    href: '#',
+                    text: 'MOVIES'
+                },
+                {
+                    href: '#',
+                    text: 'TV'
+                },
+                {
+                    href: '#',
+                    text: 'GAMES'
+                },
+                {
+                    href: '#',
+                    text: 'COLLECTIBLES'
+                },
+                {
+                    href: '#',
+                    text: 'VIDEOS'
+                },
+                {
+                    href: '#',
+                    text: 'FANS'
+                },
+                {
+                    href: '#',
+                    text: 'NEWS'
+                },
+                {
+                    href: '#',
+                    text: 'SHOP'
+                },
+                
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+    header {
+        .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
 
+        }
+    }
+    li {
+        display: inline-block;
+        padding: 1rem;
+        a {
+            text-decoration: none;
+        }
+    }
 </style>
