750052360ac99516436529d16e92a5c8ea5e02b2
MattFofo <matteo.segattini2@gmail.com>
2022-03-23 17:04:36 +0100

    stampato le card-comic

---
+++ src/components/CardComic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-comic">
+      <img :src="comicThumb" :alt="cardSeries">
+      <h2>{{ comicSeries }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComic',
+    props: {
+        comicThumb: String,
+        comicSeries: String
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/FooterDC.vue
+++ src/components/FooterDC.vue
@@ -1,31 +1,50 @@
 <template>
     <footer>
-        <div class="container">
-            <ul>
-                <h3>DC COMICS</h3>
-                <li v-for="(link, index) in linksDCComics" :key="index">
-                    {{ link.text }}
-                </li>
-                <ul>
-                    <h3>SHOP</h3>
-                    <li  v-for="(link, index) in linksShop" :key="index">
-                        {{ link.text }}
-                    </li>
-                </ul>
-            </ul>
-            <ul>
-                <h3>DC</h3>
-                <li v-for="(link, index) in linksDC" :key="index">
-                    {{ link.text }}
-                </li>
-            </ul>
-            <ul>
-                <h3>SITES</h3>
-                <li v-for="(link, index) in linksSites" :key="index">
-                    {{ link.text }}
-                </li>
-            </ul>
-            <img src="../assets/img/dc-logo-bg.png" alt="">
+        <div class="footer-links-bg">
+            <div class="container">
+                <div class="footer-links">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li v-for="(link, index) in linksDCComics" :key="index">
+                            {{ link.text }}
+                        </li>
+                        <ul>
+                            <h3>SHOP</h3>
+                            <li  v-for="(link, index) in linksShop" :key="index">
+                                {{ link.text }}
+                            </li>
+                        </ul>
+                    </ul>
+                    <ul>
+                        <h3>DC</h3>
+                        <li v-for="(link, index) in linksDC" :key="index">
+                            {{ link.text }}
+                        </li>
+                    </ul>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li v-for="(link, index) in linksSites" :key="index">
+                            {{ link.text }}
+                        </li>
+                    </ul>
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="footer-socials-bg">
+            <div class="container">
+                <div class="footer-socials">
+                    <button>join now</button>
+                    <div class="socials">
+                        <h3>FOLLOW US</h3>
+                        <img src="../assets/img/footer-facebook.png" alt="facebook-icon">
+                        <img src="../assets/img/footer-periscope.png" alt="periscope-icon">
+                        <img src="../assets/img/footer-pinterest.png" alt="pinterest-icon">
+                        <img src="../assets/img/footer-twitter.png" alt="twitter-icon">
+                        <img src="../assets/img/footer-youtube.png" alt="youtube-icon">
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>    
 </template>
@@ -178,12 +197,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    footer {
+    .footer-links-bg {
         background-image: url('../assets/img/footer-bg.jpg');
         background-size: cover;
         height: 400px;
         overflow: hidden;
-        .container {
+        position: relative;
+        .footer-links {
             display: flex;
             justify-content: space-evenly;
             img {
@@ -191,6 +211,18 @@ export default {
                 bottom: 60px;
                 margin-left: 16rem;
             }
+        }
+    }
+    .footer-socials-bg {
+        background-color: darkgrey;
+    }
+    .footer-socials {
+        display: flex;
+        justify-content: space-between;
+        .socials {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
         }
     }
 
